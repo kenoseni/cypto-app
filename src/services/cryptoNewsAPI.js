5eb7793244f4ba7a8c9f678bb52778064217f79a
Olusola Oseni <kenolusola@gmail.com>
2022-05-19 01:10:08 +0100

    implement crypto news page

---
+++ src/services/cryptoNewsAPI.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const headers = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Host": process.env.REACT_APP_BING_NEWS_API_HOST,
+  "X-RapidAPI-Key": process.env.REACT_APP_BING_NEWS_API_KEY,
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+  }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
